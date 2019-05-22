89a7d9d0a9d74ab8249b5a5b3baab820cfc027c4
Nolan Darilek <nolan@thewordnerd.info>
2019-05-22 12:49:02 -0500

    Initial commit.

---
+++ src/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <vue-announcer/>
+    <ul>
+      <li><router-link :to="{name: 'index'}">Home</router-link></li>
+      <li><router-link :to="{name: 'about'}">About</router-link></li>
+    </ul>
+    <router-view/>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import VueAnnouncer from 'vue-announcer'
+import VueMeta from 'vue-meta'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAnnouncer, {}, router)
+
+Vue.use(VueMeta)
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
